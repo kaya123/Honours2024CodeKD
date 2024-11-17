fe686f1116776d6950f9f8ee6e6c10e56daa4e52
kaya123 <kaya.dahlke@gmail.com>
2024-11-17 23:27:00 +1100

    Update isoform analysis to include batch effects test

--- ZMYND8IsoformsAcrossTime.R
+++ ZMYND8IsoformsAcrossTime.R
@@ -67,6 +67,7 @@ sigRes <- data.frame(matrix(nrow = nrow(logExpr),
 rownames(sigRes) <- rownames(logExpr)
 colnames(sigRes) <- paste(rep(c("iPSCvsD20", "D20vsD60-100"), each = 3),
                           rep(c("t", "pval", "padjusted"), 2), sep = ".")
+# check normal dist
 hist(unlist(logExpr, use.names = F))
 for (tscript in rownames(expr)) {
   print(tscript)
@@ -80,64 +81,7 @@ for (tscript in rownames(expr)) {
   # note empty NA left for the p-adjusted val
   sigRes[tscript, ] <- c(iPSCvsD20[c("t value", "Pr(>|t|)")], NA,
                          D20vsD60100[c("t value", "Pr(>|t|)")], NA)
-  #if (tscript %in% c("MICT00000218988.1","FTMT27700023654.1")) print(lmSum)
 }
-
-iPSCp <- vector()
-D20p <- vector()
-iPSCpAll <- vector()
-D20pAll <- vector()
-D60pAll <- vector()
-i <- 1
-for (tscript in rownames(expr)) {
-  curT <- t(expr)[,tscript]
-  notD60100 <- curT[grep("D[61]", colnames(expr), invert = TRUE)]
-  isiPSC <- grepl("iPSC", names(notD60100))
-  
-  notiPSC <- curT[grep("iPSC", colnames(expr), invert = TRUE)]
-  isD20 <- grepl("D20", names(notiPSC))
-  
-  
-  print(tscript)
-  # iPSC vs D20
-  w1 <- wilcox.test(formula = notD60100 ~ isiPSC)
-  # D20 vs D60+
-  w2 <- wilcox.test(formula = notiPSC ~ isD20)
-  # each category vs all others
-  w3 <- wilcox.test(formula = curT ~ grepl("iPSC", names(curT)))
-  w4 <- wilcox.test(formula = curT ~ grepl("D20", names(curT)))
-  w5 <- wilcox.test(formula = curT ~ grepl("D[16]", names(curT)))
-  
-  
-  iPSCp[i] <- w1$p.value
-  D20p[i] <- w2$p.value
-  iPSCp[is.nan(iPSCp)] <- 1
-  D20p[is.nan(D20p)] <- 1
-  
-  iPSCpAll[i] <- w3$p.value
-  D20pAll[i] <- w4$p.value
-  D60pAll[i] <- w5$p.value
-  
-  iPSCpAll[is.nan(iPSCp)] <- 1
-  D20pAll[is.nan(iPSCp)] <- 1
-  D60pAll[is.nan(D20p)] <- 1
-  i <- i + 1
-}
-
-allW <- data.frame(iPSCp,D20p, iPSCpAll, D20pAll, D60pAll)
-rownames(allW) <- rownames(expr)
-wilRes <- list()
-i <- 1
-for (test in allW) {
-  padj <- p.adjust(test, method = "BH")
-  print(colnames(allW)[i])
-  name <- colnames(allW)[i]
-  wilRes[[name]] <- rownames(expr[padj < 0.05,])
-  print(expr[padj < 0.05,])
-  i <- i + 1
-}
-
-#wilcox.test(formula = curT[,tscript] ~ (timepoint == "D20"))
 
 # adjust for multiple testing
 sigRes$iPSCvsD20.padjusted <- p.adjust(sigRes$iPSCvsD20.pval, method = "BH")
@@ -205,12 +149,76 @@ ggsave(paste0(inDir, "Kaya_thesis/isoformPheatmap.png"), device = "png",
 
 
 # using batch corrected data instead
+merged <- merge(ips, org[,-2], by="tx")
+merged$tx <- gsub("ENSG00000101040.15", "", merged$tx)
+merged$tx <- gsub("|20|-", "", merged$tx)
+merged$tx <- gsub("|", "", merged$tx, fixed=TRUE)
+rownames(merged) <- merged$tx
+colnames(merged)[-c(1,2)] <- colnames(merged[, -c(1,2)]) %>%
+  gsub("_GOK.*", "", .) %>%
+  gsub("D2.", "D20", .) %>%
+  gsub("wt", "iPSC_WT", .)
+
+merged[, -c(1,2)] <- log2(merged[, -c(1,2)] + 0.1)
+merged <- merged[, grep("WT", colnames(merged), ignore.case = TRUE)]
 batch <- c(rep(1, 3), rep(2, 9))
-adjusted_merged.wt <- removeBatchEffect(logExpr, batch = batch)
+adjusted_merged.wt <- removeBatchEffect(merged, batch = batch)
+adjusted_merged.wt <- adjusted_merged.wt[rownames(expr),]
 
 #Supp Fig
 pdf(paste0(inDir, "Kaya_thesis/supplementaryBatchEffects.pdf"), height = 5, width = 7)
-pheatmap(cor(logExpr), main = "cor(logExpr)")
-pheatmap(cor(adjusted_merged.wt), main = "cor(batch adjusted logExpr)")
+pheatmap(cor(merged[rownames(expr),]), main = "cor(logExpr)\n")
+pheatmap(cor(adjusted_merged.wt), main = "cor(batch adjusted logExpr)\n")
+pheatmap(adjusted_merged.wt, main = "batch adjusted logExpr\n")
 dev.off()
 
+timepoint <- colnames(logExpr) %>%
+  gsub("_WT.*", "", .) %>%
+  gsub("B._", "", .) %>%
+  gsub("D[61]00?", "D60-100", .) %>%
+  as.factor() 
+sigRes2 <- data.frame(matrix(nrow = nrow(adjusted_merged.wt),
+                            ncol = 6))
+rownames(sigRes2) <- rownames(adjusted_merged.wt)
+colnames(sigRes2) <- paste(rep(c("iPSCvsD20", "D20vsD60-100"), each = 3),
+                          rep(c("t", "pval", "padjusted"), 2), sep = ".")
+# check normal dist
+hist(unlist(adjusted_merged.wt, use.names = F))
+for (tscript in rownames(adjusted_merged.wt)) {
+  print(tscript)
+  tpLm <- lm(t(adjusted_merged.wt)[,tscript] ~ timepoint)
+  lmSum <- summary(tpLm)
+  D20vsiPSC <- lmSum$coefficients["timepointiPSC",]
+  # when swapping order of comparison, estimate and t val sign is swapped
+  iPSCvsD20 <- D20vsiPSC
+  iPSCvsD20[c("Estimate", "t value")] <- D20vsiPSC[c("Estimate", "t value")] * -1
+  D20vsD60100 <- lmSum$coefficients["timepointD60-100",]
+  # note empty NA left for the p-adjusted val
+  sigRes2[tscript, ] <- c(iPSCvsD20[c("t value", "Pr(>|t|)")], NA,
+                         D20vsD60100[c("t value", "Pr(>|t|)")], NA)
+}
+
+# adjust for multiple testing
+sigRes2$iPSCvsD20.padjusted <- p.adjust(sigRes2$iPSCvsD20.pval, method = "BH")
+sigRes2$`D20vsD60-100.padjusted` <- p.adjust(sigRes2$`D20vsD60-100.pval`, method = "BH")
+fwrite(sigRes2, paste0(inDir, "Kaya_thesis/lmIsoformBatchCorrected.csv"), row.names = TRUE)
+sigiPSCD20 <- rownames(sigRes2[sigRes2$iPSCvsD20.padjusted < 0.05,])
+sigD20D60 <- rownames(sigRes2[sigRes2$`D20vsD60-100.padjusted` < 0.05,])
+
+rowAnn <- data.frame(row.names = rownames(exprTr),
+                     iPSCvsD20 = factor(rownames(exprTr) %in% sigiPSCD20, levels = c("TRUE", "FALSE")),
+                     D20vsD60 = factor(rownames(exprTr) %in% sigD20D60, levels = c("TRUE", "FALSE")))
+
+
+pheatmap(adjusted_merged.wt, annotation_row = rowAnn, annotation_col = colAnn, 
+              annotation_colors = list(iPSCvsD20 = c("TRUE" = "darkgreen",
+                                                     "FALSE" = "white"),
+                                       D20vsD60 = c("TRUE" = "darkolivegreen",
+                                                    "FALSE" = "lightgrey")),
+              annotation_names_row = FALSE,
+              annotation_names_col = FALSE,
+              cutree_cols = 4,
+              clustering_callback = callback)
+ggarrange(p$table)
+ggsave(paste0(inDir, "Kaya_thesis/isoformPheatmapSupplementary.png"), device = "png",
+       height = 6, width = 7, bg = "white")
